f8cf6cf5f9849bf821b1d9c01d72be095542e018
Fa鸽 <1207247008@qq.com>
2020-02-23 02:07:52 +0800

    Framework files An argument parser to parse argument Localized help message support

---
+++ DSTEd Publisher/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DSTEd.Publisher
+{
+    public static class Program
+    {
+        static int Update(string[] commandArgs)
+        {
+            return 0;
+        }
+        static string Help_enUS = "";
+
+        public static int Main(string[] args)
+        {
+            try
+            {
+                var parser = new ArgumentParser();
+                string language = System.Globalization.CultureInfo.CurrentCulture.EnglishName;
+                try
+                {
+                    using var reader = new System.IO.StreamReader(".\\help-" + language);
+                    parser.HelpMessage = reader.ReadToEnd();
+                }
+                catch (System.IO.FileNotFoundException)
+                {
+                    parser.HelpMessage = Help_enUS;
+                    throw;
+                }
+                parser.AddHandler("update", Update);
+
+
+                return parser.Parse(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return ex.HResult;
+            }
+        }
+    }
+}
