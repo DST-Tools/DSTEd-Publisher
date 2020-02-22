891656ba8f718f6a2e0b4268c16d0fa05e6fc1dd
Adrian Preuß <adrian.preuss@icloud.com>
2020-02-22 23:20:05 +0100

    Make some cool changes

--- DSTEd Publisher/ArgumentParser.cs
+++ DSTEd Publisher/ArgumentParser.cs
@@ -15,7 +15,7 @@ namespace DSTEd.Publisher {
     }
 
     internal class ArgumentParser {
-        List<ActionClass> handlers = new List<ActionClass>(4);
+        List<ActionClass> handlers = new List<ActionClass>(900);
 
         internal void AddHandler(ActionClass handler) {
             handlers.Add(handler);
@@ -28,23 +28,21 @@ namespace DSTEd.Publisher {
 
                 if(!value.StartsWith("--") && (value[0] == '-' || value[0] == '/')) {
                     command = value.Substring(1);
+                } else if(value.StartsWith("--")) {
+                    command = value.Substring(2);
                 } else {
-                    if(value.StartsWith("--")) {
-                        command = value.Substring(2);
-                    } else {
-                        continue;
-                    }
+                    continue;
                 }
 
-                foreach(var pair in handlers) {
-                    if(string.Compare(pair.Name, command, true) == 0) {
+                foreach(var entry in handlers) {
+                    if(string.Compare(entry.Name, command, true) == 0) {
                         string[] arguments = new string[args.Length];
 
                         for(int index = 0; position < arguments.Length; position++) {
                             arguments[index] = args[index + position];
                         }
 
-                        return pair.Run(arguments);
+                        return entry.Run(arguments);
                     }
                 }
 
@@ -57,14 +55,26 @@ namespace DSTEd.Publisher {
         }
 
         private void Help() {
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("DSTEd Publisher version " + typeof(Program).Assembly.GetName().Version.ToString()  + "\n");
-            Console.WriteLine("Usage:\n");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Usage:");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Publisher.exe");
 
-            foreach (var pair in handlers) {
-                Console.WriteLine("\n\t--" + pair.Name + " " + pair.Arguments);
-                Console.WriteLine("\n\t\t" + pair.Description);
+            foreach(var entry in handlers) {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write("\n\t--" + entry.Name);
+                
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("\t" + entry.Arguments);
+                
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write("\n\t\t\t" + entry.Description);
             }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("\n");
         }
     }
 }
