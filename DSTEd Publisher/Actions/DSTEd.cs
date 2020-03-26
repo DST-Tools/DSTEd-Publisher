b26adc05f83ff1ef4d901eff43c1291e26586762
Fa鸽 <1207247008@qq.com>
2020-03-27 01:16:20 +0800

    DSTEd command base

---
+++ DSTEd Publisher/Actions/DSTEd.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSTEd.Publisher.Actions
+{
+    /// <summary>
+    /// This will be used, when DSTEd runs publisher.
+    /// </summary>
+    class DSTEd
+    {
+    }
+}
